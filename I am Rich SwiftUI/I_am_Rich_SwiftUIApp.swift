d1b3dfcc7bbc4e65281ed2230496b555bf4847d7
kantzz <codingblyatt@gmail.com>
2021-10-11 18:49:31 +0500

    Initial Commit

---
+++ I am Rich SwiftUI/I_am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich_SwiftUIApp.swift
+//  I am Rich SwiftUI
+//
+//  Created by admin on 2021-10-11.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
